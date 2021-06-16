d3349c5d8c8ed5181def07d5ba901931f689079d
debalina-infinite <79686246+debalina-infinite@users.noreply.github.com>
2021-06-16 11:33:16 +0530

    Update DemoMaven.java

--- DemoMaven.java
+++ DemoMaven.java
@@ -1,13 +1,17 @@
 package javavscode;
+import  org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testing.annotations.Test;
 
 public class DemoMaven {
     public static void main(String[] args) throws InterruptedException{
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://demo.automationtesting.in");
+        driver.findElement(By.id("email")).sendKeys("email@gmail.com");
+        driver.findElement(By.id("enterimg")).click();
         Thread.sleep(3000);
         driver.quit();
     }
